6f1352070e267189893a18c817bf2c8bc06eed05
Filip Jirsák <filip@jirsak.org>
2023-04-29 15:54:59 +0200

    BASE_PATH v odkazu na objednávku

--- src/pages/HomePage/components/Menu/index.js
+++ src/pages/HomePage/components/Menu/index.js
@@ -1,12 +1,12 @@
-import { Drink } from './components/Drink';
-import './style.css';
+import { Drink } from "./components/Drink";
+import "./style.css";
 
 export const Menu = (props) => {
   const { drinks } = props;
 
-  const element = document.createElement('section');
-  element.classList.add('menu');
-  element.id = 'menu';
+  const element = document.createElement("section");
+  element.classList.add("menu");
+  element.id = "menu";
   element.innerHTML = `
     <div class="container">
       <h2>Naše nabídka</h2>
@@ -15,16 +15,16 @@ export const Menu = (props) => {
       </p>
       <div class="drinks-list"></div>
       <div class="order-detail">
-        <a href="/objednavka">Detail objednávky</a>
+        <a href="BASE_PATH/objednavka">Detail objednávky</a>
       </div>
     </div>
   `;
 
-  if (drinks === 'loading') {
-    fetch('https://cafelora.kodim.app/api/me/drinks', {
-      method: 'GET',
+  if (drinks === "loading") {
+    fetch("https://cafelora.kodim.app/api/me/drinks", {
+      method: "GET",
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
     })
       .then((response) => response.json())
@@ -32,10 +32,10 @@ export const Menu = (props) => {
         element.replaceWith(Menu({ drinks: data.result }));
       });
   } else {
-    element.querySelector('.drinks-list').append(
-      ...drinks.map((drink) => Drink(drink))
-    );
+    element
+      .querySelector(".drinks-list")
+      .append(...drinks.map((drink) => Drink(drink)));
   }
 
   return element;
-}
+};
