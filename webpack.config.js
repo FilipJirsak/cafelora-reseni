eac71c8a767c04c9d28b9d7ca3662c14ade70997
Filip Jirsák <filip@jirsak.org>
2023-04-29 14:41:05 +0200

    test public path

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'eval-source-map',
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "eval-source-map",
   output: {
-    filename: 'bundle-[contenthash:6].js',
-    publicPath: '/',
+    filename: "bundle-[contenthash:6].js",
+    publicPath: "/cafelora-reseni",
     clean: true,
   },
   devServer: {
@@ -19,25 +19,23 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpe?g|svg|gif)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'img/[name]-[contenthash:6].[ext]'
+          filename: "img/[name]-[contenthash:6].[ext]",
         },
-      }
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: "src/index.html",
     }),
     new CopyPlugin({
-      patterns: [
-        { from: 'public', to: '', noErrorOnMissing: true },
-      ],
+      patterns: [{ from: "public", to: "", noErrorOnMissing: true }],
     }),
   ],
 };
